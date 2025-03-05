6da295976c27aad9d9637181f57b4a96f36c61f4
Alok Singh <159884599+AlokSingh100@users.noreply.github.com>
2025-03-05 16:35:00 +0530

    Merge remote-tracking branch 'origin/master'

--- project-spring/project-spring/src/main/java/newdemo/project_spring/entery/jornalEntry.java
+++ project-spring/project-spring/src/main/java/newdemo/project_spring/entery/jornalEntry.java
@@ -1,35 +1,23 @@
 package newdemo.project_spring.entery;
 
 
+import lombok.Getter;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 
-public class jornalEntry {
-
-    private long id;
+import lombok.Data;
+@Document public class jornalEntry {
+@Setter
+@Getter
+    @Id
+    private ObjectId id;
     private String title;
     private String content;
+    private LocalDateTime date;
 
-    public long getId() {
-        return id;
-    }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
